b676d67bcd8817033a3d96ec9777c7fd36169d1a
Rishav Thakker <thakker.rishav@gmail.com>
2014-01-20 01:31:26 +0530

    Added support for multiple instances of recording/streaming. Each instance is referred to by a name passed as an argument during the function call.

--- krecstream-dbus.py
+++ krecstream-dbus.py
@@ -12,38 +12,42 @@ class KRecStream(dbus.service.Object):
     def __init__(self):
         busName = dbus.service.BusName('com.rishav.KRecStream', bus = dbus.SessionBus())
         dbus.service.Object.__init__(self, busName, '/KRecStream')
+        self.recording_ids = {}
+        self.streaming_ids = {}
     
-    def record(self,w,h):
+    def record(self,w,h,name):
         print "starting"
-        os.system('rm output.flv')
-        self.recordID = subprocess.Popen("ffmpeg -video_size " + str(w) + "x" + str(h) + " -framerate 25 -f x11grab -i :0 output.flv &",shell=True)
-    def stream(self,w,h,url):
+        os.system('rm ' + name + '.flv')
+        self.recording_ids[name] = subprocess.Popen("ffmpeg -video_size " + str(w) + "x" + str(h) + " -framerate 25 -f x11grab -i :0 " + name + ".flv &",shell=True)
+        
+        
+    def stream(self,w,h,url,name):
         print "streaming"
         string = 'ffmpeg -f x11grab -s ' +  str(w) + 'x' + str(h) + ' -r 15 -i :0.0 -preset fast -pix_fmt yuv420p -threads 0 -f flv "' + url + '"'
         print string
-        self.streamID = subprocess.Popen(string + " & ", shell=True)
+        self.streaming_ids[name] = subprocess.Popen(string + " & ", shell=True)
+    
+    @dbus.service.method('com.rishav.KRecStream', in_signature = 'iiss', out_signature = '')
+    def startStream(self,w,h,url,name):
+        os.system("kdialog --passivepopup 'recording started' 3")
+        self.stream(w,h,url,name)
+        
+    @dbus.service.method('com.rishav.KRecStream', in_signature = 's', out_signature = '')
+    def stopStream(self,name):
+        print "killing " + name
+        os.system("kill " + str(self.streaming_ids[name].pid+1))
+        del self.streaming_ids[name]
     
     @dbus.service.method('com.rishav.KRecStream', in_signature = 'iis', out_signature = '')
-    def startStream(self,w,h,url):
+    def startRecord(self,w,h,name):
         os.system("kdialog --passivepopup 'recording started' 3")
-        self.stream(w,h,url)
+        self.record(w,h,name)
         
-    @dbus.service.method('com.rishav.KRecStream', in_signature = '', out_signature = '')
-    def stopStream(self):
+    @dbus.service.method('com.rishav.KRecStream', in_signature = 's', out_signature = '')
+    def stopRecord(self,name):
         print "killing"
         #self.recordID.kill()
-        os.system("kill " + str(self.streamID.pid+1))
-    
-    @dbus.service.method('com.rishav.KRecStream', in_signature = 'ii', out_signature = '')
-    def startRecord(self,w,h):
-        os.system("kdialog --passivepopup 'recording started' 3")
-        self.record(w,h)
-        
-    @dbus.service.method('com.rishav.KRecStream', in_signature = '', out_signature = '')
-    def stopRecord(self):
-        print "killing"
-        #self.recordID.kill()
-        os.system("kill " + str(self.recordID.pid+1))
+        os.system("kill " + str(self.recording_ids[name].pid+1))
         #print self.recordID.pid
     
 DBusQtMainLoop(set_as_default = True)
